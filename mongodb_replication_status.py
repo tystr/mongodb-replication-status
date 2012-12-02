05de3a9821903de366934080178ba3dd821cef4a
Tyler Stroud <ststroud@gmail.com>
2012-12-01 23:05:53 -0800

    Load configuration parameters from a config file specified in a commandline argument

--- mongodb_replication_status.py
+++ mongodb_replication_status.py
@@ -14,28 +14,39 @@ from pymongo.errors import AutoReconnect
 from time import sleep
 import smtplib
 from email.mime.text import MIMEText
+import sys
+from argparse import ArgumentParser
+from ConfigParser import RawConfigParser, NoOptionError
 
 class MongoDBReplicationStatus(object):
-    poll_interval = 5
-    lag_threshold = 30 # lag threshold in seconds
-    log_level = logging.INFO
     last_primary = None
-    max_connect_retries = 5 # number of times to attempt connecting to a hostname
+    def __init__(self, host, poll_interval=5, lag_threshold=30,
+                 max_connect_retries=5, log_level=logging.INFO,
+                 pidfile='/tmp/mongodb_replication_status.pid',
+                 logfile='/var/log/mongodb_replication_status.log'):
 
-    def __init__(self, hostnames):
+        self.poll_interval = poll_interval
+        self.lag_threshold = lag_threshold
+        self.max_connect_retries = max_connect_retries
+
         self.stdin_path = '/dev/null'
-        self.stdout_path = '/var/log/replication_status.log'
-        self.stderr_path = '/var/log/replication_status.log'
-        self.pidfile_path = '/tmp/replication_status.pid'
+        self.stdout_path = logfile
+        self.stderr_path = logfile
+        self.pidfile_path = pidfile
         self.pidfile_timeout = 5
 
-        self.hostnames = hostnames
+        self.hostnames = host
 
         self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(self.log_level)
-        self.logger_handler = logging.FileHandler('/var/log/replication_status.log')
+        self.logger.setLevel(logging.getLevelName(log_level))
+        self.logger_handler = logging.FileHandler(logfile)
         self.logger_handler.setFormatter(logging.Formatter('[%(asctime)s] %(message)s'))
         self.logger.addHandler(self.logger_handler)
+
+    def set_notifier(self, notifier):
+        assert isinstance(notifier, Notify), ('"notifier" must be an instance'
+                                             'of "Notify"')
+        self.notifier = notifier
 
     def get_members(self):
         """ Connect to the primary member and refresh the replica set status """
@@ -79,27 +90,27 @@ class MongoDBReplicationStatus(object):
         errmsg = 'ERROR: All %s attempts to connect to hostname "%s" failed. Host may be down.'\
                  % (self.max_connect_retries, hostname)
         self.logger.error(errmsg)
-        Notify().notify_alert(errmsg, '[ALERT] Host %s may be down' % hostname)
+        self.notifier.notify_alert(errmsg, '[ALERT] Host %s may be down' % hostname)
 
     def run(self):
         while True:
             members = self.get_members()
-            message = None
+            message = ''
             for member in members:
                 lag = self.get_primary_optime(members) - member['optime'].time
                 if lag > self.lag_threshold:
-                    message += 'Member "%s" is %s seconds behind the primary\n' % (member['name'], lag)
-                    self.logger.warning('WARNING: %s' % message)
+                    message += 'WARNING: Member "%s" is %s seconds behind the primary\n' % (member['name'], lag)
+                    self.logger.warning(message)
                 self.logger.debug('DEBUG: Member "%s" is %s seconds behind the primary' % (member['name'], lag))
             if message is not None:
-                notifier = Notify()
-                notifier.notify_alert(message)
+                self.notifier.notify_alert(message)
             sleep(self.poll_interval)
 
 class Notify(object):
-    smtp_host = 'localhost'
-    from_email = 'from_email'
-    recipient_emails = ['recipient1', 'recipient2']
+    def __init__(self, from_email, recipient_emails, smtp_host='localhost'):
+        self.from_email = from_email
+        self.recipient_emails = recipient_emails
+        self.smtp_host = smtp_host
 
     def notify_alert(self, message, subject='[ALERT] Replication Status Warning'):
         message = MIMEText(message)
@@ -108,7 +119,33 @@ class Notify(object):
         return mailer.sendmail(self.from_email, self.recipient_emails, str(message))
 
 if __name__ == '__main__':
-    status = MongoDBReplicationStatus(['host1:27017', 'host2:27017', 'host3:27017'])
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--config',help='The path to the configuration file', dest='FILE', required=True)
+    parser.add_argument('action', choices=('start', 'stop', 'restart'))
+    args = parser.parse_args()
+
+    config_parser = RawConfigParser()
+    config_file = open(args.FILE)
+    try:
+        config_parser.readfp(config_file)
+    finally:
+        config_file.close()
+
+    status = MongoDBReplicationStatus(
+        config_parser.get('main', 'host').split(','),
+        config_parser.getint('main', 'poll_interval'),
+        config_parser.getint('main', 'lag_threshold'),
+        config_parser.getint('main', 'max_connect_retries'),
+        config_parser.get('main', 'log_level'),
+        config_parser.get('main', 'pidfile'),
+        config_parser.get('main', 'logfile'),
+    )
+    notifier = Notify(config_parser.get('main', 'from_email'),
+                      config_parser.get('main', 'recipients'),
+                      config_parser.get('main', 'smtp_host'))
+    status.set_notifier(notifier)
+
+    sys.argv = sys.argv[0], args.action # overwrite sys.argv to be what daemon_runner expects
     daemon_runner = runner.DaemonRunner(status)
     daemon_runner.daemon_context.files_preserve = [status.logger_handler.stream]
     daemon_runner.do_action()
